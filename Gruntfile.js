afaaa7647b8d40ca5118202959c2383712e3d447
Team The Moose <the-moose@nitron.se.rit.edu>
2014-09-28 23:04:43 -0400

    Update grunt script to actually work

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,14 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    gitpull: {
-      your_target: {
-        options: {
-          // use defaults
-        }
-      }
-    },
-  });
-
-  grunt.loadNpmTasks('grunt-git');
-  grunt.registerTask('default', ['gitpull']);
-
   grunt.registerTask('poll', function () {
-    var done = this.async()
+    var done = this.async();
     var exec = require('child_process').exec;
     setInterval(function() {
-        result = grunt.task.run(['gitpull']);
-        exec('hexo generate');
-        grunt.log.writeln('Synced at ' + Date.now());
-      }, 5 * 60 * 60 * 1000); // run every five hours
+      grunt.task.run(['gitpull']);
+        exec('git pull & hexo generate', function(err, stdout, stderr) {
+	  if (err) done(err);
+          console.log(stdout);
+          grunt.log.writeln('Synced at ' + Date.now());
+       });
+     }, 5 * 60 * 60 * 1000); // run every five hours
   });
 };
