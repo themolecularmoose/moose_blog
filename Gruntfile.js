c0f543bd237905a694c9a45861958b14f8308705
bspates <bxs4361@g.rit.edu>
2014-11-08 15:06:18 -0500

    added cron for daily status

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,4 +10,21 @@ module.exports = function(grunt) {
        });
      }, 5 * 60 * 60 * 1000); // run every five hours
   });
+  grunt.registerTask('status', function () {
+    var done = this.async();
+    var request = require('request')
+    setInterval(function() {
+      request(
+        {
+          method: 'POST',
+          uri: 'https://themoose.slack.com/services/hooks/slackbot?token=97CdC9icNyveel1FFT0QCAR2&channel=%23standup',
+          body: '@channel statuses?'
+        }, function(error, response, body){
+          if(error) {
+            done(error);
+          }
+          done();
+        });
+    }, 24 * 60 * 60 * 1000); // run every once day
+  });
 };
